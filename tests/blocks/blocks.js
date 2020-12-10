95a57e3cabcb1e2cd417b6d76212b5b9b54d26cf
bychtromae <71455277+bychtromae@users.noreply.github.com>
2020-12-10 12:59:09 -0500

    block 1

--- tests/blocks/blocks.js
+++ tests/blocks/blocks.js
@@ -61,14 +61,14 @@ let blocks = {
     },
     makeGeometries: function () {
         this.flat.geom.vertices.push(
-            new THREE.Vector3(0,0,0),
-            new THREE.Vector3(1,0,0),
-            new THREE.Vector3(0,0,1),
-            new THREE.Vector3(1,0,1),
-            new THREE.Vector3(0,0.5,0),
-            new THREE.Vector3(1,0.5,0),
-            new THREE.Vector3(0,0.5,1),
-            new THREE.Vector3(1,0.5,1)
+            new THREE.Vector3(0,0,0), //0
+            new THREE.Vector3(1,0,0), //1 
+            new THREE.Vector3(0,0,1), //2
+            new THREE.Vector3(1,0,1), //3
+            new THREE.Vector3(0,0.5,0), //4
+            new THREE.Vector3(1,0.5,0), //5
+            new THREE.Vector3(0,0.5,1), //6
+            new THREE.Vector3(1,0.5,1) //7
         );
         this.flat.geom.faces.push( 
             new THREE.Face3(0,1,3),
@@ -85,5 +85,37 @@ let blocks = {
             new THREE.Face3(4,6,7)
         );
         this.flat.geom.computeFaceNormals();
+
+        this.side.geom.vertices.push(
+            new THREE.Vector3(0,0,0), //0
+            new THREE.Vector3(0.25,0,0), //1
+            new THREE.Vector3(0,0,1), //2
+            new THREE.Vector3(0.25,0,1), //3
+            new THREE.Vector3(0,0.5,0), //4
+            new THREE.Vector3(0.25,0.5,0), //5
+            new THREE.Vector3(0,0.5,1), //6
+            new THREE.Vector3(0.25,0.5,1), //7
+            new THREE.Vector3(0.5,0.25,0), //8
+            new THREE.Vector3(0.5,0.25,1) //9
+        );
+        this.side.geom.faces.push( 
+            new THREE.Face3(0,1,3),
+            new THREE.Face3(0,2,3),
+            new THREE.Face3(0,1,5),
+            new THREE.Face3(0,4,5),
+            new THREE.Face3(0,2,6),
+            new THREE.Face3(0,4,6),
+            new THREE.Face3(2,6,7),
+            new THREE.Face3(2,3,7),
+            new THREE.Face3(4,5,7),
+            new THREE.Face3(4,6,7),
+            new THREE.Face3(1,5,8),
+            new THREE.Face3(3,7,9),
+            new THREE.Face3(1,3,8),
+            new THREE.Face3(3,8,9),
+            new THREE.Face3(5,7,8),
+            new THREE.Face3(7,8,9)
+        );
+        this.side.geom.computeFaceNormals();
     }
 }
